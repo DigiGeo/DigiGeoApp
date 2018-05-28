d4b873fa41cc0ec688c157eb2ec0221bb79be611
mviencek <32376631+mviencek@users.noreply.github.com>
2018-05-27 17:46:21 -0700

    slight edit for app level permissions

--- app/src/main/java/org/digigeo/digigeo/Map.java
+++ app/src/main/java/org/digigeo/digigeo/Map.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.Manifest;
+
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,6 +33,7 @@ public class Map extends Fragment implements OnMapReadyCallback {
     private Double longitude;
     private CameraUpdate cameraUpdate;
     private SupportMapFragment mapFragment;
+    private final int REQUEST_LOCATION = 10;
 
     public Map() {
         // Required empty public constructor
@@ -40,9 +42,9 @@ public class Map extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_map, container, false);
-         mapFragment = (SupportMapFragment) getChildFragmentManager()
+        mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
-         mapFragment.getMapAsync(this);
+        mapFragment.getMapAsync(this);
         return rootView;
     }
 
@@ -51,14 +53,14 @@ public class Map extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
         locationManager = (LocationManager) this.getContext().getSystemService(this.getContext().LOCATION_SERVICE);
-        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-                showAlertPhone();
-                return;
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            showAlertPhone();
+            return;
         }
-            if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-               showAlertApp();
-                return;
-            }
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 100, locationListenerNetwork);
             Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             mMap.setMyLocationEnabled(true);
@@ -69,26 +71,76 @@ public class Map extends Fragment implements OnMapReadyCallback {
                 cameraUpdate = CameraUpdateFactory.newLatLngZoom(myPosition, 16);
                 mMap.animateCamera(cameraUpdate);
             }
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
-            @Override
-            public void onMapClick(LatLng point) {
+                @Override
+                public void onMapClick(LatLng point) {
 
-                MarkerOptions marker = new MarkerOptions().position(
-                        new LatLng(point.latitude, point.longitude)).title("New Cache");
+                    MarkerOptions marker = new MarkerOptions().position(
+                            new LatLng(point.latitude, point.longitude)).title("New Cache");
 
-                mMap.addMarker(marker);
+                    mMap.addMarker(marker);
+                }
+            });
+
+            //listener and click event for removing markers
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    marker.remove();
+                    return true;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 100, locationListenerNetwork);
+                        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        mMap.setMyLocationEnabled(true);
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            myPosition = new LatLng(latitude, longitude);
+                            cameraUpdate = CameraUpdateFactory.newLatLngZoom(myPosition, 16);
+                            mMap.animateCamera(cameraUpdate);
+                        }
+                        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+                            @Override
+                            public void onMapClick(LatLng point) {
+
+                                MarkerOptions marker = new MarkerOptions().position(
+                                        new LatLng(point.latitude, point.longitude)).title("New Cache");
+
+                                mMap.addMarker(marker);
+                            }
+                        });
+
+                        //listener and click event for removing markers
+                        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                            @Override
+                            public boolean onMarkerClick(Marker marker) {
+                                marker.remove();
+                                return true;
+                            }
+                        });
+                    }
+
+                }
             }
-        });
-
-        //listener and click event for removing markers
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                marker.remove();
-                return true;
+            default:{
             }
-        });
+
+        }
     }
 
     @Override
@@ -111,23 +163,6 @@ public class Map extends Fragment implements OnMapReadyCallback {
         //todo:figure out how to close dialog and extract string resources
 
     }
-
-    private void showAlertApp() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this.getContext());
-        dialog.setTitle("Enable Location Permission For App")
-                .setMessage("Set Permissions")
-                .setPositiveButton("Approve", (paramDialogInterface, paramInt) -> {
-                    Intent myIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                    Uri uri = Uri.fromParts("package",this.getContext().getPackageName(), null);
-                    myIntent.setData(uri);
-                    startActivityForResult(myIntent, 0);
-                    paramDialogInterface.cancel();
-                })
-                .setNegativeButton("Cancel", (paramDialogInterface, paramInt) -> {});
-        dialog.show();
-        //todo:figure out how to close dialog and extract string resources
-    }
-
 
     private final LocationListener locationListenerNetwork = new LocationListener() {
         public void onLocationChanged(Location location) {
