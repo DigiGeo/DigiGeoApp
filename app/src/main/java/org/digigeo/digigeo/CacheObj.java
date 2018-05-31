d3d0adc399350fd3ae537a010b6234a56b9c5453
Clint <1054484+cjshaw@users.noreply.github.com>
2018-05-30 20:05:47 -0700

    nothing real

---
+++ app/src/main/java/org/digigeo/digigeo/CacheObj.java
@@ -0,0 +1,58 @@
+package org.digigeo.digigeo;
+
+public class CacheObj {
+
+    private String content;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public CacheObj(String name, String content, double latitude, double longitude) {
+        this.name = name;
+        this.content = content;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "CacheObj{" +
+                "content='" + content + '\'' +
+                ", name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
---
+++ app/src/main/java/org/digigeo/digigeo/CreateCache.java
@@ -0,0 +1,46 @@
+package org.digigeo.digigeo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CreateCache extends Fragment {
+
+    private EditText cacheName;
+    private EditText cacheContents;
+    private Button submit;
+
+
+    public CreateCache() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_create_cache, container, false);
+
+        cacheName = rootView.findViewById(R.id.cacheName);
+        cacheContents = rootView.findViewById(R.id.cacheContent);
+        submit = rootView.findViewById(R.id.submitBtn);
+
+        return rootView;
+    }
+
+    public CacheObj createCache(View v) {
+
+        String name = cacheName.getText().toString();
+        String contents = cacheContents.getText().toString();
+
+        //TODO get current Lat and Long from map fragment
+        double lat = 47.6062; // dummy lat
+        double lon = 122.3321; //dummy long
+
+        CacheObj cache = new CacheObj(name, contents, lat, lon);
+
+        return cache;
+    }
+}
